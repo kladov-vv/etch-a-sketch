c416f69e200bee5ca24231f3eb1a9597301ff197
kladov-vv <kladov.vv@yandex.com>
2024-11-14 14:27:42 +1000

    Fix class spelling, move grid div creation to html from js

--- code.js
+++ code.js
@@ -1,13 +1,8 @@
-const body = document.querySelector('body');
-const grid = document.createElement('div');
-
-grid.classList.add('.grid');
-
-body.appendChild(grid);
+const grid = document.querySelector('.grid');
 
 for (let i = 0; i < 16; i++) {
     const square = document.createElement('div');
-    square.classList.add('.square');
+    square.classList.add('square');
     grid.appendChild(square);
     console.log(i);
 }
