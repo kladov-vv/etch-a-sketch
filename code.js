230a79b8697a043dc18eebd690a6f7ad2b802186
kladov-vv <kladov.vv@yandex.com>
2024-11-15 00:20:25 +1000

    Add maximum grid size and add initial board 16x16

--- code.js
+++ code.js
@@ -1,10 +1,15 @@
 const grid = document.querySelector('.grid');
 const btn = document.querySelector('button');
 
-
 grid.addEventListener('mouseover', draw);
 btn.addEventListener('click', newBoard);
 
+for (let i = 0; i < (16 * 16); i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${100 / 16}%`
+    grid.appendChild(square);
+}
 
 function draw(event) {
     let target = event.target; 
@@ -14,14 +19,18 @@ function draw(event) {
 }
 
 function newBoard() {
-    let size = prompt();
-    while(grid.firstChild) {
-         grid.removeChild(grid.lastChild);
-    }
-    for (let i = 0; i < (size * size); i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.width = `${100 / size}%`
-        grid.appendChild(square);
+    let size = prompt('ENTER THE GRID SIZE');
+    if(size > 100 || size < 0) {
+        newBoard();
+    } else {
+        while(grid.firstChild) {
+            grid.removeChild(grid.lastChild);
+       }
+       for (let i = 0; i < (size * size); i++) {
+           const square = document.createElement('div');
+           square.classList.add('square');
+           square.style.width = `${100 / size}%`
+           grid.appendChild(square);
+       }
     }
 }
