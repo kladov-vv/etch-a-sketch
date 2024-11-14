616dec9c8ee51a9140e389c39168996c1bc30933
kladov-vv <kladov.vv@yandex.com>
2024-11-14 15:07:17 +1000

    Change index title

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     grid.appendChild(square);
