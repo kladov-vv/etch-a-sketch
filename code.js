111a705845d340fda0229e98432aabc2a44b0fac
kladov-vv <kladov.vv@yandex.com>
2024-11-14 22:14:26 +1000

    New layout with header and main parts

--- code.js
+++ code.js
@@ -4,4 +4,14 @@ for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     grid.appendChild(square);
+    // square.addEventListener('mouseenter', draw);
+}
+
+// grid.addEventListener('mouseover', draw);
+
+function draw(event) {
+    let target = event.target; 
+    if (target.className === 'square') {
+        event.target.style.backgroundColor = 'red';
+    }
 }
