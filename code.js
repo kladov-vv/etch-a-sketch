1105c48de4a7d5ec05a8a711bd6f8251550d9682
kladov-vv <kladov.vv@yandex.com>
2024-11-14 14:05:07 +1000

    Adding classes to div boxes

--- code.js
+++ code.js
@@ -1,10 +1,14 @@
 const body = document.querySelector('body');
 const grid = document.createElement('div');
-const square = document.createElement('div');
+
+grid.classList.add('.grid');
 
 body.appendChild(grid);
+
 for (let i = 0; i < 16; i++) {
     const square = document.createElement('div');
+    square.classList.add('.square');
     grid.appendChild(square);
+    console.log(i);
 }
 
