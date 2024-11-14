27682f1ee9a054192c8ded0aa8e3d542780e41f1
kladov-vv <kladov.vv@yandex.com>
2024-11-15 00:10:00 +1000

    Add function for creating new board

--- code.js
+++ code.js
@@ -1,17 +1,27 @@
 const grid = document.querySelector('.grid');
+const btn = document.querySelector('button');
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    grid.appendChild(square);
-    // square.addEventListener('mouseenter', draw);
-}
 
-// grid.addEventListener('mouseover', draw);
+grid.addEventListener('mouseover', draw);
+btn.addEventListener('click', newBoard);
+
 
 function draw(event) {
     let target = event.target; 
-    if (target.className === 'square') {
+    if (target.className == 'square') {
         event.target.style.backgroundColor = 'red';
+    }
+}
+
+function newBoard() {
+    let size = prompt();
+    while(grid.firstChild) {
+         grid.removeChild(grid.lastChild);
+    }
+    for (let i = 0; i < (size * size); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${100 / size}%`
+        grid.appendChild(square);
     }
 }
